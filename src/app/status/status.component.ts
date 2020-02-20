59319d3e5a153983fb57f887b25af5c88e6f5ba0
Queisler, Yannik <Yannik.Queisler@kvwl.de>
2020-02-20 17:34:37 +0100

    Change scsss

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -9,7 +9,8 @@ import {Step} from '../shared/model/step.model';
 export class StatusComponent implements OnInit {
 
   steps = [new Step('Auf Warteliste', 'lorem', false, true),
-    new Step('Auf fsdgssdf', 'lorem', true, false)];
+    new Step('Auf fsdgssdf', 'lorem', true, false),
+    new Step('Auf sdfg', 'lorem', false, false)];
 
   constructor() { }
 
