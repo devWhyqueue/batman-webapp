e4a0427509dedf9045b20c37cf9d07c1f80b23c9
devWhyqueue <dev.yannik.queisler@gmail.com>
2020-03-06 16:26:18 +0100

    Fix schema

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   authServer: 'https://batman-auth.herokuapp.com/api/',
   registrationServer: 'https://batman-registration.herokuapp.com/api/',
-  mailServer: 'http://batman-mail.herokuapp.com/api/'
+  mailServer: 'https://batman-mail.herokuapp.com/api/'
 };
