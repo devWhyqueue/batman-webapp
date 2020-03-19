911908c4e5295350b3a79c73686db900af6b0e02
Queisler, Yannik <Yannik.Queisler@kvwl.de>
2020-03-19 16:12:06 +0100

    Add cookie notice

---
+++ src/cookie-notice-config.js
@@ -0,0 +1,14 @@
+new cookieNoticeJS({
+  // Localizations of the notice message
+  'messageLocales': {
+    'de': 'Auch wir verwenden Cookies - das ist nichts Schlimmes. Sie erleichtern die Benutzung der Seite.'
+  },
+  // Dismiss button background color
+  'buttonBgColor': '#6c757d',
+  // Dismiss button text color
+  'buttonTextColor': '#f8f9fa',
+  // Notice background color
+  'noticeBgColor': '#23304a',
+  // Notice text color
+  'noticeTextColor': '#f8f9fa',
+});
